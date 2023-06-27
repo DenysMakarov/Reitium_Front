7868c267c09b74fa090efda874387f8341cf02a0
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2023-06-27 14:44:49 -0600

    all testes react have done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import 'h8k-components';
 import UserList from './components/UserList';
 import AddEditUser from './components/AddEditUser';
 
-const title = 'User Management'
+const title = 'User Management';
 
 const App = () => {
-    const initialState = { firstName: '', lastName: '', phone: '' }
+    const initialState = { firstName: '', lastName: '', phone: '' };
+    const [users, setUsers] = useState([]);
+    const [editIndex, setEditIndex] = useState(-1);
+    const [formData, setFormData] = useState(initialState);
+
+    const handleDeleteUser = (index) => {
+        const updatedUsers = [...users];
+        updatedUsers.splice(index, 1);
+        setUsers(updatedUsers);
+    };
+
+    const handleEditUser = (index) => {
+        setEditIndex(index);
+        const user = users[index];
+        setFormData(user);
+    };
+
+    const handleCancelEdit = () => {
+        setEditIndex(-1);
+        setFormData(initialState);
+    };
+
+    const handleAddEditUser = (user) => {
+        if (editIndex !== -1) {
+            const updatedUsers = [...users];
+            updatedUsers[editIndex] = user;
+            setUsers(updatedUsers);
+            setEditIndex(-1);
+        } else {
+            setUsers((prev) => [...prev, user]);
+        }
+        setFormData(initialState);
+    };
 
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
-            <div className='layout-row justify-content-center mt-100'>
-                <div className='w-60 mr-75'>
-                    <UserList />
+            <div className="layout-row justify-content-center mt-100">
+                <div className="w-60 mr-75">
+                    <UserList users={users} onDelete={handleDeleteUser} onEdit={handleEditUser} />
                 </div>
-                <div className='layout-column w-40'>
-                    <AddEditUser />
+                <div className="layout-column w-40">
+                    <AddEditUser formData={formData} setFormData={setFormData} onCancelEdit={handleCancelEdit} onAddEditUser={handleAddEditUser} />
                 </div>
             </div>
         </div>
     );
-}
+};
+
 
 export default App;
+
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,52 +1,47 @@
 import React from 'react'
 
-function UserList() {
 
-    
+function UserList({ users, onDelete, onEdit }) {
+    const handleDelete = (index) => {
+        console.log('Delete user:', users[index]);
+        onDelete(index);
+    };
+
+    const handleEdit = (index) => {
+        onEdit(index);
+    };
+
     return (
         <section>
-            <h3 className='p-3 text-center'>Users</h3>
-            <table className="table table-striped table-bordered" data-testid='userListTable'>
+            <h3 className="p-3 text-center">Users</h3>
+            <table className="table table-striped table-bordered" data-testid="userListTable">
                 <thead>
-                    <tr>
-                        <th>
-                            First Name
-                        </th>
-                        <th>
-                            Last Name
-                        </th>
-                        <th>
-                            Phone Number
-                        </th>
-                        <th>
-                            Actions
-                        </th>
-                    </tr>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Phone Number</th>
+                    <th>Actions</th>
+                </tr>
                 </thead>
                 <tbody>
-                    <tr>
+                {users.map((user, index) => (
+                    <tr key={index}>
+                        <td>{user.firstName}</td>
+                        <td>{user.lastName}</td>
+                        <td>{user.phone}</td>
                         <td>
-                            Lorem
-                        </td>
-                        <td>
-                            Ipsum
-                        </td>
-                        <td>
-                            1234567890
-                        </td>
-                        <td>
-                            <button type='button'>
+                            <button type="button" onClick={() => handleEdit(index)}>
                                 Edit
                             </button>
-                            <button type='button'>
+                            <button type="button" onClick={() => handleDelete(index)}>
                                 Delete
                             </button>
                         </td>
                     </tr>
+                ))}
                 </tbody>
             </table>
         </section>
     );
 }
-
 export default UserList;
